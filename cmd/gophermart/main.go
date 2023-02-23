911057ba6d131c2bf9fd8aaa6277d5dd27ccc3b4
Gleb Brozhe <brojeg@yandex.com>
2023-02-23 14:28:48 -0800

    First commit

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,37 @@
 package main
 
-func main() {}
+import (
+	"diploma/go-musthave-diploma-tpl/internal/controllers"
+	model "diploma/go-musthave-diploma-tpl/internal/models"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/user/new", controllers.CreateAccount).Methods("POST")
+	router.HandleFunc("/api/user/login", controllers.Authenticate).Methods("POST")
+	router.HandleFunc("/api/contacts/new", controllers.CreateContact).Methods("POST")
+	router.HandleFunc("/api/me/contacts", controllers.GetContactsFor).Methods("GET") //  user/2/contacts
+
+	router.Use(model.JwtAuthentication) //attach JWT auth middleware
+
+	//router.NotFoundHandler = app.NotFoundHandler
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000" //localhost
+	}
+
+	fmt.Println(port)
+
+	err := http.ListenAndServe(":"+port, router) //Launch the app, visit localhost:8000/api
+	if err != nil {
+		fmt.Print(err)
+	}
+}
