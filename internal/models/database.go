aa6f94deb5cd75bb2b3dd415d8b585282b72d991
Gleb Brozhe <brojeg@yandex.com>
2023-02-27 11:54:48 -0800

    added logger breakpints

--- internal/models/database.go
+++ internal/models/database.go
@@ -15,6 +15,7 @@ func GetDB() *gorm.DB {
 
 	conn, err := gorm.Open("postgres", connectionString)
 	if err != nil {
+		logger.Error(connectionString)
 		logger.Errorf("Error is %e \n Connection string is %s", err, connectionString)
 	}
 
