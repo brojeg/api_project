0d3280d34ebe27b6db7eab1cf96675ce46b44fbf
Gleb Brozhe <brojeg@yandex.com>
2023-02-27 16:37:07 -0800

    test

--- internal/controllers/accrualController.go
+++ internal/controllers/accrualController.go
@@ -3,23 +3,34 @@ package controllers
 import (
 	"context"
 	"diploma/go-musthave-diploma-tpl/internal/models"
-	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"time"
 )
 
-func RequestAccrual(endpont, orderid string) *[]models.Accrual {
-	accrual := &[]models.Accrual{}
+func RequestAccrual(endpont, orderid string) *models.Accrual {
+	accrual := &models.Accrual{}
 	URL := endpont + "/api/orders/" + orderid
 	resp, err := http.Get(URL)
 	if err != nil {
 		logger.Error(err)
-		return nil
+		return accrual
 	}
-	errDecode := json.NewDecoder(resp.Body).Decode(accrual)
-	if errDecode != nil {
-		logger.Error(err)
+	// errDecode := json.NewDecoder(resp.Body).Decode(accrual)
+	// if errDecode != nil {
+	// 	logger.Error(err)
+	// }
+	body, error := ioutil.ReadAll(resp.Body)
+	if error != nil {
+		fmt.Println(error)
 	}
+	// close response body
+	resp.Body.Close()
+
+	// print response body
+	fmt.Println(string(body))
+
 	return accrual
 }
 
@@ -40,14 +51,14 @@ func ApplyAccruals(ctx context.Context, interval, accrualURL string) {
 				// order.changeOrderStatus("REGISTERED")
 				// order.changeOrderStatus("PROCESSING")
 				order := models.GetOrderByNumber(order.Number)
-				models.GetBalance(order.UserID)
+				balance := models.GetBalance(order.UserID)
 				accrual := RequestAccrual(accrualURL, order.Number)
 				if accrual != nil {
-					// order.Accrual = float64(accrual.Accrual)
-					// order.Status = accrual.Status
-					// balance.Add(order.Accrual, order.UserID)
-					// order.Save()
-					// balance.Save()
+					order.Accrual = float64(accrual.Accrual)
+					order.Status = accrual.Status
+					balance.Add(order.Accrual, order.UserID)
+					order.Save()
+					balance.Save()
 				}
 
 				// order.changeOrderStatus("PROCESSED")
